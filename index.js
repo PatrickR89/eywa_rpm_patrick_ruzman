39446353e1cdb05bdb8e03e594cc1c318938508d
PatrickR89 <patrickr.89sc@gmail.com>
2022-06-10 08:21:19 +0200

    Set basic functionality to access web-browser

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const webdriver = require("selenium-webdriver");
+const { logger } = require("./logger");
 const By = webdriver.By;
 const until = webdriver.until;
 const companies = require("./input.json");
-const driver = new webdriver.Builder()
-  .forBrowser("chrome")
-  .usingServer("http://localhost:4444/wd/hub")
-  .build();
-// Show window in full screen
-driver.manage().window().maximize();
-driver.get("https://www.eywaonline.com/development");
+// const driver = new webdriver.Builder()
+//     .forBrowser("firefox")
+//     .usingServer("http://localhost:4444/wd/hub")
+//     .build();
 
-driver
-  .sleep(300)
-  .then(() => {
-    // Write EYWA login code here
-  })
-  .then(() => {
-    // Write code that opens module "Kontakti" here
-  })
-  .then(() => {
-    // Write next step code here etc.
-  });
+const firefox = require("selenium-webdriver/firefox");
+
+const options = new firefox.Options();
+const service = new firefox.ServiceBuilder().build();
+const driver = firefox.Driver.createSession(options, service);
+
+logger.info("linkedIn_worker SCRIPT START");
+
+const getTitile = async () => {
+    // let driver = await new webdriver.Builder()
+    //     .forBrowser(webdriver.Browser.FIREFOX)
+    //     .usingServer("http://localhost:4444/wd/hub")
+    //     .build();
+
+    try {
+        await driver.get("https://www.google.com/ncr");
+        await driver
+            .findElement(By.name("q"))
+            .sendKeys("webdriver", Key.RETURN);
+        await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+        await driver.quit();
+    } catch (err) {
+        logger.info(err);
+    }
+};
+
+try {
+    // driver.manage().window().maximize();
+    // driver.get("https://www.eywaonline.com/development");
+    // driver
+    //     .sleep(300)
+    //     .then(() => {
+    //         // Write EYWA login code here
+    //     })
+    //     .then(() => {
+    //         // Write code that opens module "Kontakti" here
+    //     })
+    //     .then(() => {
+    //         // Write next step code here etc.
+    //     });
+    getTitile();
+} catch (err) {
+    console.log(err);
+    logger.info(err);
+}
+
+logger.info("linkedIn_worker SCRIPT END \n");
--- logger.js
+++ logger.js
@@ -1,20 +1,22 @@
 const winston = require("winston");
 
-const messageFormat = print(({ timestamp, level, message }) => {
-    return `${timestamp}[${level}]: ${message}`;
+const messageFormat = winston.format.printf(({ timestamp, level, message }) => {
+    return `[${timestamp}]${level}: ${message}`;
 });
 
-export const logger = winston.createLogger({
+const logger = winston.createLogger({
     level: "info",
-    format: format.combine(
-        format.timestamp({
+    format: winston.format.combine(
+        winston.format.timestamp({
             format: "DD-MM-YYYY HH:mm:ss"
         }),
         messageFormat
     ),
     transports: [
         new winston.transports.File({
-            filename: application.log
+            filename: "application.log"
         })
     ]
 });
+
+module.exports.logger = logger;
